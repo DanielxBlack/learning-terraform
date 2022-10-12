7485151815a0be43b876972b1e532c36384cb5b1
danielxblack <nuxie@nux.com>
2022-10-12 04:28:18 +0000

    updated main

--- 2_chapter/webserver/main.tf
+++ 2_chapter/webserver/main.tf
@@ -17,27 +17,42 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world." > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
 
   tags = {
-    Name = "terrafart"
+    Name = "terror_form_halloween_edition"
   }
 }
 
 # AWS does not allow traffic in or out of EC2 instances by defualt.
 # You have to conifgure a security group.
 
+# First, let's set a variable.
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests."
+  type = number
+  default = 8080
+} 
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "This is the public IP of my AWS EC2 Instance."
+
 }
 
