886a4bf3bc9d02cc4f70666a8f04237acc147d03
danielxblack <nuxie@nux.com>
2022-10-13 03:29:38 +0000

    added health checks and load balancer

--- 2_chapter/webserver_cluster/main.tf
+++ 2_chapter/webserver_cluster/main.tf
@@ -52,6 +52,9 @@ resource "aws_autoscaling_group" "example" {
     # We pass it data, which we use the aws_subnets listed below
     vpc_zone_identifier = data.aws_subnets.default.ids
 
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     # We set a min and max size. Defaults to 2.
     min_size = 2
     max_size = 10
@@ -83,6 +86,93 @@ resource "aws_security_group" "instance" {
     }
 }
 
+
+# Let's create the load balancer resource:
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type =  "application"
+    subnets = data.aws_subnets.default.ids
+    # We need to assign the security group
+    security_groups = [aws_security_group.alb.id]
+}
+
+# Then, we create a target group for our ASG using the aws_lb_target_group resource
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+# Now we define a listener for the ALB (Application Load Balancer)
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    # By default, return a simple 404 page
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: Page Not Found"
+        status_code = 404
+      }
+    }
+}
+
+# By default, AWS resources, including ALBs do NOT allow incoming
+# or outgoing traffic. You need to create a new security group
+# specifically for the ALB.
+
+resource "aws_security_group" "alb" {
+    name = "terraform-example-alb"
+
+    # Allow inbound HTTP requests
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Allow all outbound requests
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+    condition {
+        path_pattern {
+          values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+
 # We can call a data source. The aws_vpc 
 # default will show us our provider (aws_vpc)
 # The name we want to refer to it as (default)
@@ -102,3 +192,8 @@ data "aws_subnets" "default" {
     }
 }
 
+
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer."
+}
